59572368bbcd6c3960e04e3afddc5b21bcf8f355
KoMOKWENA <konlukhanyo@gmail.com>
2023-09-09 09:16:34 -0700

    C variables if, els, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int f;
+
+for (f = 48; f < 58; f++)
+{
+putchar(i);
+}
+for (f = 97; f < 103; f++)
+{
+putchar(f);
+}
+putchar('\n');
+return (0);
+}
