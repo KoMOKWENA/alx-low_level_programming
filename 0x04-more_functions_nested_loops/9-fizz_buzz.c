e9a14f49edafc26ed369989e0e37c0bd079d013f
KoMOKWENA <konlukhanyo@gmail.com>
2023-09-14 06:24:30 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,18 +15,22 @@ int c;
 for (c = 1; c <= 100; c++)
 {
 if (c % 3 == 0 && c % 5 != 0)
- {
+{
 printf(" Fizz");
-} else if (c % 5 == 0 && c % 3 != 0)
+}
+else if (c % 5 == 0 && c % 3 != 0)
 {
 printf(" Buzz");
-} else if (c % 3 == 0 && c % 5 == 0)
+}
+else if (c % 3 == 0 && c % 5 == 0)
 {
 printf(" FizzBuzz");
-} else if (c == 1)
+}
+else if (c == 1)
 {
 printf("%d", c);
-} else
+}
+else
 {
 printf(" %d", c);
 }
