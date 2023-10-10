e39a17f261d47a5d1f59106e7068b27250e0f31f
KoMOKWENA <konlukhanyo@gmail.com>
2023-10-10 12:28:15 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+
+/**
+ *File: 4-sum
+ *@x: first input value
+ *@y: second input value 
+ *Desco: the sum of x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
