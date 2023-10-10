26cabadc487374aa6bb6172556f25e56c2583867
KoMOKWENA <konlukhanyo@gmail.com>
2023-10-10 10:15:38 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for the dog
+ * @d: struct dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+ree(d->owner);
+free(d);
+}
+}
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,36 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a dog struct
+ * @name: name of the dog 
+ * @age: the age of the dog 
+ * @owner: name of the owner 
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+char *_strcpy(char *dest, char *src);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+
+#endif
+
