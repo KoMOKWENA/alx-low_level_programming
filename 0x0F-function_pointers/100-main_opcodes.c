ddbe17307b5051fb16ca87a8bc93482cd006b4dd
KoMOKWENA <konlukhanyo@gmail.com>
2023-10-11 11:49:30 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int bytes, o;
+char *arr;
+
+
+
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+
+
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+
+
+
+arr = (char *)main;
+
+for (o = 0; o < bytes; o++)
+{
+if (o == bytes - 1)
+{
+printf("%02hhx\n", arr[o]);
+break;
+}
+printf("%02hhx ", arr[o]);
+}
+return (0);
+}
+
