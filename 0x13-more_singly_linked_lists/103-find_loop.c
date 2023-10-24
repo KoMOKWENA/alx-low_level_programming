9533e5d8093abb15220ca62de2bcdb66eb89ccf7
KoMOKWENA <konlukhanyo@gmail.com>
2023-10-24 14:40:43 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -5,7 +5,7 @@
  * find_listint_loop - finds the loop in a linked list
  * @head: linked list to search for
  *
- * Return: address of the node 
+ * Return: address of the node
  */
 
 listint_t *find_listint_loop(listint_t *head)
