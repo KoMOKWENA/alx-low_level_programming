356d829ce5d120c50114432c2aa678bdcec340f7
KoMOKWENA <konlukhanyo@gmail.com>
2023-10-26 12:09:47 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *print_binary - print binary representing a number 
+ *print_binary - print binary representing a number
  * @n: num to print in binary
- * 
+ *
  */
 
 void print_binary(unsigned long int n)
 {
 if (n > 1)
 
-print_binary (n >> 1);
+print_binary(n >> 1);
 _putchar ((n & 1) + '0');
 }
 
