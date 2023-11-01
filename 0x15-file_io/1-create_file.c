41c8db686c6f695e61890e722ce56e71c51a3f60
KoMOKWENA <konlukhanyo@gmail.com>
2023-11-01 12:25:07 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,44 +1,39 @@
 #include "main.h"
 
 /**
-* create_file - creates a file
-* @filename: Name of the file
-* @text_content: content writed in the file.
-*
-* Return: 1 if it success. -1 if fails.
-*/
-
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-int nletters;
-int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
+	if (!filename)
+		return (-1);
 
-if (!filename)
-return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	if (fd == -1)
+		return (-1);
 
-if (fd == -1)
-return (-1);
+	if (!text_content)
+		text_content = "";
 
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-if (!text_content)
-text_content = "";
+	rwr = write(fd, text_content, nletters);
 
+	if (rwr == -1)
+		return (-1);
 
-for
-{
-(nletters = 0; text_content[nletters]; nletters++);
-rwr = write(fd, text_content, nletters);
-}
+	close(fd);
 
-if (rwr == -1)
-return (-1);
-
-
-close(fd);
-return (1);
+	return (1);
 }
 
